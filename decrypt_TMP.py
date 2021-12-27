6a47582533177988f05d44481fa53724d86f0207
rrmarinho <32780523+rrmarinho@users.noreply.github.com>
2021-12-27 07:54:58 -0300

    Add files via upload

---
+++ decrypt_TMP.py
@@ -0,0 +1,43 @@
+import sys
+import re
+
+if len(sys.argv) < 3:
+    print("ERR: informe o arquivo TMP e o arquivo de saida")
+    sys.exit(1)
+
+input_file = open(sys.argv[1], 'r')
+output_file = sys.argv[2]
+
+buff = []
+key_code = []
+
+while (True):
+    line = input_file.readline()
+
+    if not line:
+        break
+
+    rgx_buff = re.match("byte\[\] buff = new byte\[\] {(.*)}", line)
+
+    if (rgx_buff):
+        buff = rgx_buff.group(1).split(',')
+
+
+    rgx_key = re.match("byte\[\] key_code = new byte\[\] {(.*)}", line)
+
+    if (rgx_key):
+        key_code = rgx_key.group(1).split(',')
+
+
+decrypted = []
+j = 0
+for i in range(len(buff)):
+    decrypted.append(int(buff[i],16) ^ int(key_code[j],16))
+    j = j + 1
+    if (j >= len(key_code)):
+            j = 0
+
+
+with open(output_file, "wb") as f:
+    f.write(bytearray(decrypted))
+
